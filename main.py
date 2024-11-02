01e075846be6ff75767ebd922325d5c5bfbd32f0
Florian Baillien <124878216+florianbaillien@users.noreply.github.com>
2024-11-02 20:43:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import tkinter as tk
 
+def ascii_to_dec():
+    text = entry.get()
+    result = chr((sum(ord(char) for char in text) % 64) + 64) + "\\r"
+    text_result.delete(1.0, tk.END)
+    text_result.insert(tk.END, text + result)
+
+root = tk.Tk()
+
+label_prompt = tk.Label(root, text="Entrez le texte :")
+label_prompt.pack()
+
+entry = tk.Entry(root)
+entry.pack()
+
+button = tk.Button(root, text="Calculer", command=ascii_to_dec)
+button.pack()
+
+text_result = tk.Text(root, height=2, width=30)
+text_result.pack()
+
+root.mainloop()
